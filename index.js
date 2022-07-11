9eb847bdbccc603e0bd184af5f0e71edeca3d7df
Alec Avery <jalecavery@gmail.com>
2022-07-11 19:11:05 -0400

    bunch of updates

---
+++ index.js
@@ -0,0 +1,27 @@
+// var videoDiv;
+// var radio1;
+// var radio2;
+// var radio3;
+// var radio4;
+// var radio5;
+
+// // Run code after the window is loaded
+// window.onload = function() {
+//     videoDiv = document.getElementById('video')
+//     radio1 = document.getElementById('r1');
+//     radio2 = document.getElementById('r2');
+//     radio3 = document.getElementById('r3');
+//     radio4 = document.getElementById('r4');
+//     radio5 = document.getElementById('r5');
+//     slideShow();
+// }
+
+// function slideShow() {
+//     if(document.getElementById('r1').checked) {
+//         document.getElementById('video').style.right = '0px';
+//     } if (document.getElementById('r2').checked) {
+//         document.getElementById('video').style.right = '100vw';
+//     }
+// }
+
+// slideShow();
